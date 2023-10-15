3950f2ca7739e7b440da6725de6e1db0b750df9a
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2023-10-15 20:23:39 +0100

    Botão Reset e sua funcionalidade

--- js.js
+++ js.js
@@ -2,6 +2,8 @@ const billValue = document.querySelector('#bill-value');
 const btn = document.getElementsByClassName('btn-percentagem');
 const numberOfPeople = document.querySelector('#number-of-people');
 const tipAmountText = document.querySelector('#tip-amount-text');
+const totalPorPerson = document.querySelector('#total-person-text');
+const valorCustomizado = document.querySelector('#valor-Customizado');
 
 for (let index = 0; index < btn.length; index += 1) {
   btn[index].addEventListener('click', () => {  
@@ -13,3 +15,13 @@ for (let index = 0; index < btn.length; index += 1) {
         tipAmountText.innerText = `$${tipAmount.toFixed(2)}`;
     });
 };
+
+//Resetando todos os dados
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', () => {
+    billValue.value = 0;
+    numberOfPeople.value = 0;
+    tipAmountText.innerText = '$0.00';
+    totalPorPerson.innerText = '$0.00';
+    valorCustomizado.value = 'Custom';
+});
